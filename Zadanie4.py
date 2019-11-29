3999d097e902d3d47e5494e1a42a2784bd0bd397
arkakrak <arkakrak@outlook.com>
2019-11-29 18:02:01 +0100

    v1

--- Zadanie4.py
+++ Zadanie4.py
@@ -1,25 +1,26 @@
-"""Proszę
-sprawdzić
-czas
-potrzebny
-na
-przeszukanie
-poniższej
-listy
-pod
-kątem - 1.
-Proszę
-zastosować
-wszystkie
-sposoby
-przeszukania, które
-przyjdą
-do
-głowy.
-
-list comprehention na wykładzie
-"""
-
+import time
 from random import randint
 
-long_list = [randint(0, 3000) for element in range(1000000)]
+ranger = 1000000
+long_list = [randint(0, 3000) for element in range(ranger)]
+
+timer_start = time.perf_counter()
+for x in range(ranger):
+    if long_list == (-1):
+        print("-1 found!")
+
+timer_stop = time.perf_counter()
+timer_total = timer_stop - timer_start
+print("Timer start: ", timer_start)
+print("Timer end: ", timer_stop)
+print("Timer reading: ", timer_total)
+
+timer_start = time.perf_counter()
+if (-1) not in long_list:
+    print("-1 is not on the list")
+else:
+    print("-1 found!")
+timer_stop = time.perf_counter()
+print("Timer start: ", timer_start)
+print("Timer end: ", timer_stop)
+print("Timer reading: ", timer_total)
