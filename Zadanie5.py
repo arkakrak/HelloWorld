3486fde9d74be316e271816b91d32536bb805520
arkakrak <arkakrak@outlook.com>
2019-11-29 18:08:01 +0100

    v1

---
+++ Zadanie5.py
@@ -0,0 +1,4 @@
+names_list = ['Damian', 'Ola', 'Barbara', 'Robert', 'Zygmunt', 'Ewa']
+
+first_letter = [name[0] for name in names_list]
+print("List of the first letters of names on the list: ", first_letter)
