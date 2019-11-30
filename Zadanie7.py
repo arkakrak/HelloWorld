231b5247d3b3e4676394766bb5e4ca4fb2e3b715
arkakrak <arkakrak@outlook.com>
2019-11-30 10:27:55 +0100

    v5 - final

--- Zadanie7.py
+++ Zadanie7.py
@@ -23,6 +23,7 @@ def my_first_function(*nothing_important, width, length, height):
     print("width: {}".format(width))
     print("length: {}".format(length))
     print("height: {}".format(height))
+    print("*****************************")
 
 
 my_first_function("helicopter", width=80, length=20, height=180)
@@ -32,9 +33,27 @@ def my_second_function(width, length, *nothing_important):
     print("width: {}".format(width))
     print("length: {}".format(length))
     print("nothing_important: {}".format(nothing_important))
-
+    print("*****************************")
 
 my_second_function(80, 30, "battle helicopter")
-# def my_third_function(something, **kwargs):
 
-# def my_fourth_function(**kwargs, something):
+
+def my_third_function(length, height, **nothing_important):
+    print("length: {}".format(length))
+    print("height: {}".format(height))
+    print("nothing_important: {}".format(nothing_important))
+    print("*****************************")
+
+
+my_third_function(300, 200, gender="scout_helicopter")
+
+# code below doesn't work - kwargs needs to be the last
+"""
+def my_fourth_function(**nothing_important, name):
+    print("nothing_important: {}".format(nothing_important))
+    print("name: {}".format(name))
+    print("*****************************")
+    
+    
+my_fourth_function(gender="tranport helicopter", Chinook)
+"""
