61f6813871b8b773a8dac4326250d36768bca844
arkakrak <39877139+arkakrak@users.noreply.github.com>
2019-12-12 13:35:11 +0100

    v3

--- Zadanie10.py
+++ Zadanie10.py
@@ -1,10 +1,19 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from time import sleep
 
 browser = webdriver.Firefox()
 browser.maximize_window()
 browser.get("https://duckduckgo.com/")
-browser.find_element_by_id("search_form_input_homepage").send_keys("Harmonogram ii up")
-browser.find_element_by_id("search_button_homepage").click()
-browser.find_element_by_id("r1-0").click()
-assert "Harmonogramy" in browser.title
-# browser.find_element_by_link_text("pdf/schedules/2019-20/niestacjonarne/II rok/zjazd7_13-15_XII_INF II N 2019-20.pdf").click()
+try:
+    browser.find_element_by_id("search_form_input_homepage").send_keys("Harmonogram ii up")
+    browser.find_element_by_id("search_button_homepage").click()
+    browser.find_element_by_id("r1-0").click()
+    assert "Harmonogramy" in browser.title
+    sleep(10)
+    browser.find_element_by_link_text("Zjazd 7 13-15 XII 2019 r.").click()
+except NoSuchElementException:
+    print("No such element in html code")
+finally:
+    sleep(10)
+    browser.close()
